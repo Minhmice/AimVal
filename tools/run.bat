2b1854eb96438abe945e2ca032fb66b9ab9e0a49
Minhmice <trantueminh35@gmail.com>
2025-09-26 21:05:07 +0700

    Fix decode issue, improve OpenGL PBO upload performance

--- tools/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-REM Short wrapper to run the tracker quietly
-python -m aimval_tracker --overlay --scale 0.8 --log-level warn %*
-
--- tools/run.ps1
+++
@@ -1,2 +0,0 @@
-# Short wrapper to run the tracker quietly
-python -m aimval_tracker --overlay --scale 0.8 --log-level warn $args
